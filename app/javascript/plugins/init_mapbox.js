d5fee34d10a595a9946e7f6d21bbeb441ec76a17
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2019-09-06 16:59:43 +0100

    correct map and navbars

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,13 +2,13 @@ import mapboxgl from 'mapbox-gl';
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
-  if (markers > 1){
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  if (markers.length > 1){
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
   else { bounds.extend([ markers.lng, markers.lat ]);
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-}}
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  }}
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
