81daac685cd078c2b813330b54bc839b18d25f66
Pedro Andrade <pedrocbva@gmail.com>
2019-09-09 23:04:30 +0100

    até amanhã

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,12 @@ class GroupsController < ApplicationController
     @groups = policy_scope(Group)
     if params.dig(:search, :location).present? && params.dig(:search, :start_time).present? && params.dig(:search, :end_time).present? && params.dig(:search, :date).present?
       @groups = Group.search(params.dig(:search, :location), params.dig(:search, :start_time), params.dig(:search, :end_time), params.dig(:search, :date))
-
+      if params.dig(:search, :parking).present? || params.dig(:search, :indoor).present? || params.dig(:search, :locker_room).present? || params.dig(:search, :field_size).present? || params.dig(:search, :field_type).present?
+        # filtered_results
+        # fields_groups
+        @resolts = Field.by_filter(params[:parking], params[:indoor], params[:locker_room], params[:field_type], params[:field_size])
+        @resolts
+      end
       @markers = @groups.map do |group|
         {
           lat: group.field.latitude,
@@ -17,9 +22,58 @@ class GroupsController < ApplicationController
       end
     else
       flash[:alert] = "You must fill all search fields"
-      redirect_to root_path
+      render pages: "home"
     end
   end
+
+  # def filtered_results
+
+  #   if params.dig(:search, :parking).present?
+  #     @fields = Field.parking
+  #   elsif params.dig(:search, :parking).present? && params.dig(:search, :indoor).present?
+  #     @fields = Field.parking.indoor
+  #   elsif params.dig(:search, :parking).present? && params.dig(:search, :indoor).present? && params.dig(:search, :locker_room).present?
+  #     @fields = Field.parking.indoor.locker_room
+  #   elsif params.dig(:search, :parking).present? && params.dig(:search, :indoor).present? && params.dig(:search, :locker_room).present? && params.dig(:search, :field_size).present?
+  #     @fields = Field.parking.indoor.locker_room.field_size(params.dig(:search, :field_size))
+  #   elsif params.dig(:search, :parking).present? && params.dig(:search, :indoor).present? && params.dig(:search, :locker_room).present? && params.dig(:search, :field_size).present? && params.dig(:search, :field_type).present?
+  #     @fields = Field.parking.indoor.locker_room.field_size(params.dig(:search, :field_size)).field_type(params.dig(:search, :field_type))
+  #   elsif params.dig(:search, :parking).present? && params.dig(:search, :indoor).present? && params.dig(:search, :locker_room).present? && params.dig(:search, :field_type).present?
+  #     @fields = Field.parking.indoor.locker_room.field_type(params.dig(:search, :field_type))
+  #   elsif params.dig(:search, :parking).present? && params.dig(:search, :locker_room).present?
+  #     @fields = Field.parking.locker_room
+  #   elsif params.dig(:search, :parking).present? && params.dig(:search, :locker_room).present? && params.dig(:search, :field_size).present?
+  #     @fields = Field.parking.locker_room.field_size(params.dig(:search, :field_size))
+  #   elsif params.dig(:search, :parking).present? && params.dig(:search, :locker_room).present? && params.dig(:search, :field_size).present? && params.dig(:search, :field_type).present?
+  #     @fields = Field.parking.locker_room.field_size(params.dig(:search, :field_size)).field_type(params.dig(:search, :field_type))
+  #   elsif params.dig(:search, :parking).present? && params.dig(:search, :locker_room).present? && params.dig(:search, :field_type).present?
+  #     @fields = Field.parking.locker_room.field_type(params.dig(:search, :field_type))
+  #   elsif params.dig(:search, :indoor).present?
+  #     @fields = Field.indoor
+  #   elsif params.dig(:search, :indoor).present? && params.dig(:search, :locker_room).present?
+  #     @fields = Field.indoor.locker_room
+  #   elsif params.dig(:search, :indoor).present? && params.dig(:search, :locker_room).present? && params.dig(:search, :field_size).present?
+  #     @fields = Field.indoor.locker_room.field_size(params.dig(:search, :field_size))
+  #   elsif params.dig(:search, :indoor).present? && params.dig(:search, :locker_room).present? && params.dig(:search, :field_size).present? && params.dig(:search, :field_type).present?
+  #     @fields = Field.indoor.locker_room.field_size(params.dig(:search, :field_size)).field_type(params.dig(:search, :field_type))
+  #   elsif params.dig(:search, :indoor).present? && params.dig(:search, :locker_room).present? && params.dig(:search, :field_type).present?
+  #     @fields = Field.indoor.locker_room.field_type(params.dig(:search, :field_type))
+  #   elsif params.dig(:search, :locker_room).present?
+  #     @fields = Field.locker_room
+  #   elsif params.dig(:search, :locker_room).present? && params.dig(:search, :field_size).present?
+  #     @fields = Field.locker_room.field_size(params.dig(:search, :field_size))
+  #   elsif params.dig(:search, :locker_room).present? && params.dig(:search, :field_size).present? && params.dig(:search, :field_type).present?
+  #     @fields = Field.locker_room.field_size(params.dig(:search, :field_size)).field_type(params.dig(:search, :field_type))
+  #   elsif params.dig(:search, :locker_room).present? && params.dig(:search, :field_type).present?
+  #     @fields = Field.locker_room.field_type(params.dig(:search, :field_type))
+  #   elsif params.dig(:search, :field_size).present?
+  #     @fields = Field.field_size(params.dig(:search, :field_size))
+  #   elsif params.dig(:search, :field_size).present? && params.dig(:search, :field_type).present?
+  #     @fields = Field.field_size(params.dig(:search, :field_size)).field_type(params.dig(:search, :field_type))
+  #   else
+  #     @fields = Field.field_type(params.dig(:search, :field_type))
+  #   end
+  # end
 
   def show
     @group = Group.find(params[:id])
@@ -31,6 +85,12 @@ class GroupsController < ApplicationController
         # infoWindow: render_to_string(partial: "info_window", locals: { group: group })
       }
   end
+
+  # def fields_groups
+  #   fields_ids = @fields.pluck(:id)
+  #   @groups = Group.where(field: [fields_ids])
+  #   @groups
+  # end
 
   def create
     @group = Group.new(group_params)
@@ -57,7 +117,7 @@ class GroupsController < ApplicationController
         @group.change_status!
         redirect_to my_bookings_path
       else
-        redirect_to group_path(@group), alert: "You are already in that group! Fuck You Nigga!"
+        redirect_to group_path(@group), alert: "You are already in that group!"
       end
     else
       redirect_to group_path(@group)
@@ -77,6 +137,4 @@ class GroupsController < ApplicationController
   def check_if_player
     redirect_to root_path, alert: "Action not allowed!" unless current_user.Player?
   end
-
-
 end
--- app/models/field.rb
+++ app/models/field.rb
@@ -3,7 +3,7 @@ class Field < ApplicationRecord
   validates :location, presence: true
   validates :description, presence: true
   validates :field_size, presence: true, inclusion: { in: [3, 5, 7, 11] }
-  validates :field_type, presence: true, inclusion: { in: ["Grass", "Synthetic", "Concrete", "Wood", "Sand"] }
+  validates :field_type, presence: true, inclusion: { in: ["Grass", "Synthetic", "Concrete", "Wood"] }
   validates :parking, inclusion: { in: [true, false] }
   validates :parking, exclusion: { in: [nil] }
   validates :locker_room, inclusion: { in: [true, false] }
@@ -18,11 +18,44 @@ class Field < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 
+  scope :parking, -> { where(parking: true) }
+  scope :locker_room, -> { where(locker_room: true) }
+  scope :indoor, -> { where(indoor: true) }
+  scope :field_size, ->(field_size) { where("field_size = ?", field_size) }
+  scope :field_type, ->(field_type) { where("field_type = ?", field_type) }
+
   def self.search(location, start_time, end_time, date)
     booked_field_ids = Group.between_dates(start_time, end_time, date).where(status: :Booked).pluck(:field_id)
     pending_field_ids = Group.between_dates(start_time, end_time, date).where(status: :Pending).pluck(:field_id)
     near(location, 20)
       .where(id: pending_field_ids)
       .where.not(id: booked_field_ids)
+  end
+
+  def self.by_filter(parking = nil, indoor = nil, locker_room = nil, field_size = nil, field_type = nil)
+    return where(parking: parking) if parking
+    return where(parking: parking, indoor: indoor) if parking && indoor
+    return where(parking: parking, indoor: indoor, locker_room: locker_room) if parking && indoor && locker_room
+    return where(parking: parking, indoor: indoor, locker_room: locker_room, field_size: field_size) if parking && indoor && locker_room && field_size
+    return where(parking: parking, indoor: indoor, locker_room: locker_room, field_size: field_size, field_type: field_type) if parking && indoor && locker_room && field_size && field_type
+    return where(parking: parking, indoor: indoor, locker_room: locker_room, field_type: field_type) if parking && indoor && locker_room && field_type
+    return where(parking: parking, locker_room: locker_room) if parking && locker_room
+    return where(parking: parking, locker_room: locker_room, field_size: field_size) if parking && locker_room && field_size
+    return where(parking: parking, locker_room: locker_room, field_size: field_size, field_type: field_type) if parking && locker_room && field_size && field_type
+    return where(parking: parking, locker_room: locker_room, field_type: field_type) if parking && locker_room && field_type
+    return where(indoor: indoor) if indoor
+    return where(indoor: indoor, locker_room: locker_room) if indoor && locker_room
+    return where(indoor: indoor, locker_room: locker_room, field_size: field_size) if indoor && locker_room && field_size
+    return where(indoor: indoor, locker_room: locker_room, field_size: field_size, field_type: field_type) if indoor && locker_room && field_size && field_type
+    return where(indoor: indoor, locker_room: locker_room, field_type: field_type) if indoor && locker_room && field_type
+    return where(locker_room: locker_room) if locker_room
+    return where(locker_room: locker_room, field_size: field_size) if locker_room && field_size
+    return where(locker_room: locker_room, field_size: field_size, field_type: field_type) if locker_room && field_size && field_type
+    return where(locker_room: locker_room, field_type: field_type) if locker_room && field_type
+    return where(field_size: field_size) if field_size
+    return where(field_size: field_size, field_type: field_type) if field_size && field_type
+    return where(field_type: field_type) if field_type
+
+    all
   end
 end
