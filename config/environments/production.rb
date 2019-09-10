c986a9662754ac8f70d7d8a361efc5b72ac051bd
Pedro Candeias <pedromiguelcandeias@gmail.com>
2019-09-10 15:34:56 +0100

    solved redis thing -- Install redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = "wss://www.footylewagon.club/cable"
+  config.action_cable.allowed_request_origins = [ "http://www.footylewagon.club", "https://www.footylewagon.club" ]
 end
