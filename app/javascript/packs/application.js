3abe9bb558fe7411ccbfd960fb6d1f1bf49a8b08
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2019-09-11 11:54:31 +0100

    add field and field index done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,13 +5,11 @@ import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { navAction } from '../plugins/navbar_slide';
 import { navScroll } from '../plugins/scroll_navbar';
-import { hoverCard } from '../plugins/hover_card';
 import { navNoLogWithScroll } from '../plugins/nav_no_log_with_scroll';
 import { checkbox } from '../plugins/checkbox';
 
 initMapbox();
 initAutocomplete();
-hoverCard();
 checkbox();
 
 const navbar = document.querySelector(".footy-navbar")
